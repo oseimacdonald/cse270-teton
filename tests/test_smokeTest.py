874e1cb299c044d35e90ac529eb62c56e1d278ee
oseimacdonald <oseimacdonald@gmail.com>
2025-08-09 23:45:47 +0000

    Smoke Test - 2

--- tests/test_smokeTest.py
+++ tests/test_smokeTest.py
@@ -9,16 +9,16 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options
 
 class TestSmokeTest():
   def setup_method(self, method):
     options = Options()
     options.add_argument("--headless = new")
-    # Add Firefox-specific preferences
+    # Add Chrome-specific preferences
     options.set_preference("dom.webdriver.enabled", False)
     options.set_preference("useAutomationExtension", False)
-    self.driver = webdriver.Firefox(options=options)
+    self.driver = webdriver.Chrome(options=options)
     self.vars = {}
   
   def teardown_method(self, method):
